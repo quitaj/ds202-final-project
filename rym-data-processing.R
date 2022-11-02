b20d5913cb422911aacc2b025ea89c1266f19f40
wagstrom <sorenbwagstrom@gmail.com>
2022-11-01 21:04:16 -0500

    changed to github link

--- rym-data-processing.R
+++ rym-data-processing.R
@@ -7,15 +7,15 @@ df$Number.of.Ratings = as.numeric(gsub(",", "", df$Number.of.Ratings))
 df$Ranking = as.numeric(df$Ranking)
 df$Average.Rating = as.numeric(df$Average.Rating)
 df$Number.of.Reviews = as.numeric(df$Number.of.Reviews)
-
 df2 = df
+
 #Converting comma-separated strings into separate columns
 df2[c('Genre_1', 'Genre_2', 'Genre_3', 'Genre_4', 'Genre_5')] <- str_split_fixed(df2$Genres, ', ', 5)
 df2[c('Desc_1', 'Desc_2', 'Desc_3', 'Desc_4', 'Desc_5', 'Desc_6', 'Desc_7', 'Desc_8',
      'Desc_9', 'Desc_10')] <- str_split_fixed(df2$Descriptors, ', ', 10)
 sum(df2$Desc_10 != "")
-
 df3 = df2
+
 #Add column for average rating, average reviews, average ratings, and number of appearances per artist
 df3 = df3 %>%
   group_by(Artist.Name) %>%
@@ -23,9 +23,6 @@ df3 = df3 %>%
   mutate(Artist.Average.Reviews = mean(Number.of.Reviews)) %>%
   mutate(Artist.Average.Ratings = mean(Number.of.Ratings)) %>%
   mutate(Artist.Frequency = nrow(as.data.frame(Artist.Name)))
-
-View(df3)
-
 df_final = df3
 
 #Summaries
