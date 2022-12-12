ca6bcf7e2a383ee8d039b50e5b2dd32eef763675
joliejac <joliejac@iastate.edu>
2022-12-11 19:55:41 -0600

    results 1 and conclusion completed. added the intial data cleaning and summaries code to the rym-data-processing file

--- rym-data-processing.R
+++ rym-data-processing.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(stringr)
+library(dplyr)
 df = read.csv("https://raw.githubusercontent.com/wagstrom/ds-202-final-project/main/rym_top_5000_all_time.csv")
 
 #Converting to numeric
@@ -29,6 +30,30 @@ View(df3)
 
 df_final = df3
 
+Year_useful = str_sub(df_final$Release.Date,-2,-1)
+Year_start = NULL
+for(i in 1:5000){
+  if(Year_useful[i] <  22){
+    Year_start[i] = 20
+  }else{
+    Year_start[i] = 19
+    }}
+    year = data.frame(Year_start, Year_useful)
+    df_final$Year = paste(year$Year_start, year$Year_useful)
+    df_final$Year = gsub(" ", "", df_final$Year)
+    Month = gsub("[^a-zA-Z]", "",df_final$Release.Date )
+    df_final$Month = Month
+    Day = parse_number(str_sub(df_final$Release.Date,,2))
+    df_final$Day=factor(Day)
+    
+   # In this chunk we are adding columns to find the averages of scores,
+  #  reviews, number of ratings, and number of appearances for each artist.
+   # We do this so we can compare the individual values easier.
+
+View(df_final)
+  df_final <- df_final %>% select(!(Release.Date|Genres|Descriptors))  
+  write.csv(df_final,"df_final.csv",row.names = FALSE)
+  
 #Summaries
 nrow(unique(as.data.frame(df_final$Album)))
   #Some albums have the same names
@@ -37,5 +62,6 @@ nrow(unique(as.data.frame(df_final$Artist.Name)))
 nrow(unique(as.data.frame(df_final$Genre_1)))
   #453 unique primary genres
 
-
+summary(df_final)
+str(df_final)
 
